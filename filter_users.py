8cdb1aaf9bfc0f3ebde3a57f99fcb8dd270d3a50
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-26 13:46:39 +0200

    Add filtering by email to the feature branch

--- filter_users.py
+++ filter_users.py
@@ -10,6 +10,7 @@ def filter_users_by_name(name):
     for user in filtered_users:
         print(user)
 
+
 def filter_users_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
@@ -18,6 +19,17 @@ def filter_users_by_age(age):
 
     for user in filtered_users:
         print(user)
+
+
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+
 
 if __name__ == "__main__":
     filter_option = input("What would you like to filter by? (age or name): ").strip().lower()
